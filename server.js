bfa7f5ad481e45c4e9b05c773ea1258d73c3a0e6
raphaeldiaz <mrraphaeldiaz@gmail.com>
2021-03-10 15:46:19 -0800

    updates pages

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var data = require('./data/test.json');
 
 var app = express();
 const PORT = 3000;
@@ -11,6 +12,27 @@ app.get('/', (req,res)=>{
   res.render('index',{title:title});
 });
 
+//add users route
+app.get('/users', function(req, res) {
+	var title = 'Users Page';
+	res.render('users/index', {
+    	title: title,
+    	users: data
+	});
+});
+
+//add user/view route - we are cheating by using the array index + 1
+app.get('/users/view/:id', function(req, res) {
+  var title = 'User Page';
+  var id = req.params.id;
+  res.render('users/view', {
+      title: title,
+      user: data[--id]
+  });
+ });
+ 
+ 
 app.listen(PORT,()=>{
-  console.log(`App is running on port ${PORT}`);
+ console.log(`App is running on port ${PORT}`);
+console.log(data)
 });
